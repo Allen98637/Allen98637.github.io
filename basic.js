c907d55ef94f4b4b3234804c70edf4cc35a457cb
Allen98637 <asfasfasf986375@gmail.com>
2022-06-25 22:25:48 +0800

    ahaha

--- basic.js
+++ basic.js
@@ -42,22 +42,11 @@ if(menu1.className != "menuItem chosenItem"){
 }
 const menu2 = document.getElementById("menu2");
 var menu2Event = null;
+var menu2sub = document.getElementById("menu2sub");
+var menu2subEvent;
+var menu2subScroll = true;
 if(menu2.className != "menuItem chosenItem"){
     menu2.style.borderTopWidth = "1px";
-    menu2.addEventListener('mouseover', function () {
-        clearInterval(menu2Event);
-        menu2Event = setInterval(function(){
-            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)+1+"px";
-            if(parseInt(menu2.style.borderTopWidth) >= 5){menu2.style.borderTopWidth = "5px";clearInterval(menu2Event);}
-        }, 20);
-    });
-    menu2.addEventListener('mouseout', function () {
-        clearInterval(menu2Event);
-        menu2Event = setInterval(function(){
-            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)-1+"px";
-            if(parseInt(menu2.style.borderTopWidth) <= 1){menu2.style.borderTopWidth = "1px";clearInterval(menu2Event);}
-        }, 20);
-    });
 }
 const menu3 = document.getElementById("menu3");
 var menu3Event = null;
@@ -93,5 +82,100 @@ function frame() {
     icons.style.opacity = op;
     if(op == 1){
         clearInterval(introa);
+    }
+}
+
+
+const menu2sub0 = document.getElementById("menu2sub0");
+const menu2sub1 = document.getElementById("menu2sub1");
+const menu2sub2 = document.getElementById("menu2sub2");
+
+menu2.onmouseover = function(){
+    menu2sub.style.display = "block";
+    if(menu2subScroll){
+        clearInterval(menu2subEvent);
+        clearInterval(menu2Event);
+        menu2Event = setInterval(function(){
+            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)+1+"px";
+            menu2sub.style.top = 56 - parseInt(menu2.style.borderTopWidth) +"px";
+            if(parseInt(menu2.style.borderTopWidth) >= 5){
+                menu2.style.borderTopWidth = "5px";
+                menu2sub.style.top = "51px";
+                clearInterval(menu2Event);
+            }
+        }, 20);
+        menu2subEvent = setInterval(menu2subGo,20);
+        menu2subScroll = false;
+    }
+};
+menu2sub.onmouseover = function(){
+    menu2sub.style.display = "block";
+    if(menu2subScroll){
+        clearInterval(menu2subEvent);
+        clearInterval(menu2Event);
+        menu2Event = setInterval(function(){
+            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)+1+"px";
+            menu2sub.style.top = 56 - parseInt(menu2.style.borderTopWidth) +"px";
+            if(parseInt(menu2.style.borderTopWidth) >= 5){
+                menu2.style.borderTopWidth = "5px";
+                menu2sub.style.top = "51px";
+                clearInterval(menu2Event);
+            }
+        }, 20);
+        menu2subEvent = setInterval(menu2subGo,20);
+        menu2subScroll = false;
+    }
+};
+menu2.onmouseout = function(){
+    if(!menu2subScroll && !menu2sub.matches(":hover")){
+        clearInterval(menu2subEvent);
+        clearInterval(menu2Event);
+        menu2Event = setInterval(function(){
+            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)-1+"px";
+            menu2sub.style.top = 56 - parseInt(menu2.style.borderTopWidth) +"px";
+            if(parseInt(menu2.style.borderTopWidth) <= 1){
+                menu2.style.borderTopWidth = "1px";
+                menu2sub.style.top = "55px";
+                clearInterval(menu2Event);
+            }
+        }, 20);
+        menu2subEvent = setInterval(menu2subBack,20);
+        menu2subScroll = true;
+    }
+};
+menu2sub.onmouseout = function(){
+    if(!menu2subScroll && !menu2.matches(":hover")){
+        clearInterval(menu2subEvent);
+        clearInterval(menu2Event);
+        menu2Event = setInterval(function(){
+            menu2.style.borderTopWidth = parseInt(menu2.style.borderTopWidth)-1+"px";
+            menu2sub.style.top = 56 - parseInt(menu2.style.borderTopWidth) +"px";
+            if(parseInt(menu2.style.borderTopWidth) <= 1){
+                menu2.style.borderTopWidth = "1px";
+                menu2sub.style.top = "55px";
+                clearInterval(menu2Event);
+            }
+        }, 20);
+        menu2subEvent = setInterval(menu2subBack,20);
+        menu2subScroll = true;
+    }
+};
+
+var menu2subY = 0;
+function menu2subGo(){
+    menu2subY = menu2subY + (170 - menu2subY) * 0.2;
+    menu2sub.style.top = menu2subY - 110 + "px";
+    if(menu2subY >= 166){
+        menu2sub.style.top = "60px";
+        clearInterval(menu2subEvent);
+    }
+}
+function menu2subBack(){
+    menu2subY *= 0.8;
+    menu2sub.style.top = menu2subY - 110 + "px";
+    if(menu2subY < 5){
+        menu2sub.style.top = "-110px";
+        menu2sub.style.display = "none";
+        clearInterval(menu2subEvent);
     }
 }
