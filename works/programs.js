988e1e6df04897a0de807f1b0ee62e0d9bb57df6
Allen98637 <asfasfasf986375@gmail.com>
2022-07-09 14:00:08 +0800

    BC roll

--- works/programs.js
+++ works/programs.js
@@ -8,5 +8,15 @@ const data = [
         "titl":[0,255,255],
         "font":[0,0,0],
         "href":"https://top.gg/bot/723497172821213204"
+    },
+    {
+        "name":"Battle Cats Gacha",
+        "desc":"A discord bot that simulates rolling Battle Cats capsules",
+        "img":"BCroll.webp",
+        "color":[255,255,0],
+        "scolor":[255,255,150],
+        "titl":[0,100,100],
+        "font":[0,0,255],
+        "href":"https://top.gg/bot/481973540317560832"
     }
 ]
